7974135dcc7249f7ff9cc5e1d3353f4d338335fb
fibonacci <799974017@qq.com>
2021-10-24 20:54:14 +0800

    命令模式

--- src/main/java/com/fibonacci/pattern/command/demo/ConcreteCommand.java
+++ src/main/java/com/fibonacci/pattern/command/demo/ConcreteCommand.java
@@ -1,14 +1,16 @@
 package com.fibonacci.pattern.command.demo;
 
 /**
+ * 具体命令类
+ *
  * @author fibonacci
  * @date 2021-10-23 22:41
  */
-public class ConcreteCommand implements Command{
+public class ConcreteCommand implements Command {
 
     private Receiver receiver;
 
-    public ConcreteCommand(){
+    public ConcreteCommand() {
         this.receiver = new Receiver();
     }
 
--- src/main/java/com/fibonacci/pattern/command/demo/Receiver.java
+++ src/main/java/com/fibonacci/pattern/command/demo/Receiver.java
@@ -1,12 +1,14 @@
 package com.fibonacci.pattern.command.demo;
 
 /**
+ * 实现者/接收者
+ *
  * @author fibonacci
  * @date 2021-10-23 22:41
  */
 public class Receiver {
 
-    public void action(){
+    public void action() {
         System.out.println("接受者action被调用");
     }
 
