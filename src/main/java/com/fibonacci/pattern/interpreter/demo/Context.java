ccc8a1f1797ec8e5c6134b7ca4175ee84f901d32
fibonacci <799974017@qq.com>
2021-10-29 22:46:19 +0800

    解释器模式

--- src/main/java/com/fibonacci/pattern/interpreter/demo/Context.java
+++ src/main/java/com/fibonacci/pattern/interpreter/demo/Context.java
@@ -11,11 +11,11 @@ public class Context {
     private AbstractExpression exp;
 
     public Context() {
-        //数据初始化
+        // 数据初始化
     }
 
     public void operation(String info) {
-        //调用相关表达式类的解释方法
+        // 调用相关表达式类的解释方法
     }
 
 }
